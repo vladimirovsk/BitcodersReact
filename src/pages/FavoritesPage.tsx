48e05771eca752d472657c8968559b79db66e679
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-12-29 13:38:49 -0700

    add github api

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {useAppSelector} from '../hooks/redux';
 
 export function FavoritesPage() {
+	const { favorites } = useAppSelector(state=> state.github)
+
+	if (favorites.length === 0 ) return <p className='text-center'>No item</p>
+
 	return (
 		<div className='flex pl-28 pt-4'>
-			Favorites pages
+			<div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+				<ul className='list-none'>
+					{ favorites.map(f=>(
+						<li key={f}>
+							<a href={f} target='_blank'>{f}</a>
+						</li>
+					)
+					)}
+				</ul>
+			</div>
 		</div>
 	)
 }
