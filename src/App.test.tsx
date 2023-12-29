1cd62499d92a26b1322281e0fdf00a56fad30a24
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-12-29 10:13:24 -0700

    add pages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,4 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import {Provider} from 'react-redux';
-import {store} from './store';
-import {BrowserRouter} from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(
-      <Provider store ={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-    </Provider>
-  );
-  const linkElement = screen.getByText(/Github Search/i);
-  expect(linkElement).toBeInTheDocument();
 });
