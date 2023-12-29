af0cfdfd9412dfe103b5c065806af1464ef06c8f
Serhii Vladimirov <vladimirovsk@gmail.com>
2023-12-29 14:33:03 -0700

    Refactoring Docker

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Header = () => {
-	const leftBlock = (<div className="flex"><h2>Test UserName</h2></div>)
+	const leftBlock = (<div className="flex"><h2>UserName</h2></div>)
 	return (
 		<div className='bg-gray-100 flex justify-between p-4 border-2 border-gray-200'>
 			{leftBlock}
