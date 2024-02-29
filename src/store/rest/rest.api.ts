67702f5169fade119112795c47faf54c7446fc19
Serhii Vladimirov <vladimirovsk@gmail.com>
2024-02-29 10:14:43 -0700

    fix host

--- src/store/rest/rest.api.ts
+++ src/store/rest/rest.api.ts
@@ -1,10 +1,11 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {AuthData, AuthResponse} from './auth.interface';
+import * as process from 'process';
 
 export const restApi = createApi({
 	reducerPath: 'api/v1',
 	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://api-rest.bitcoders.net/api/v1',
+		baseUrl: process.env.REACT_APP_API_URL //'https://api-rest.bitcoders.net/api/v1',
 	}),
 	endpoints: build => ({
 		login:  build.query<AuthResponse, AuthData>({
